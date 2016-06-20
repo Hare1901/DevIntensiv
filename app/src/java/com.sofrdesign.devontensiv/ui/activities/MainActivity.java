61ab10f12e5f93423eab7084862d2aa0f422fdf4
Илья Садчиков < >
2016-06-20 23:48:27 +0300

    Initial commit

---
+++ app/src/java/com.sofrdesign.devontensiv/ui/activities/MainActivity.java
@@ -0,0 +1,12 @@
+package com.sofrdesign.devontensiv.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
